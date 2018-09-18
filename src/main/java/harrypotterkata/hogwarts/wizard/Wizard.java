6f1ddfc5163a83821864a4d497edb126b0c2ada3
xanvranck <vranckaert.xan@gmail.com>
2018-09-18 18:05:30 +0200

    deleted static import, so you can see the sorting hat (awesome)

--- src/main/java/harrypotterkata/hogwarts/wizard/Wizard.java
+++ src/main/java/harrypotterkata/hogwarts/wizard/Wizard.java
@@ -1,14 +1,12 @@
 package harrypotterkata.hogwarts.wizard;
 
-import static harrypotterkata.hogwarts.wizard.SortingHat.determineHouseForWizard;
-
 public class Wizard {
     private String name;
     private House houseOfHogwarts;
 
     private Wizard(String name) {
         this.name = name;
-        this.houseOfHogwarts = determineHouseForWizard();
+        this.houseOfHogwarts = SortingHat.determineHouseForWizard();
     }
 
     public static Wizard createWizardlyWizard(String name) {
