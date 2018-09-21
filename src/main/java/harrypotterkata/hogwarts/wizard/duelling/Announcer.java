655fb1591bb559117c203348a150de78fbc807c8
wimlens <wim.lens@cegeka.be>
2018-09-21 19:27:52 +0200

    introduce announcer

---
+++ src/main/java/harrypotterkata/hogwarts/wizard/duelling/Announcer.java
@@ -0,0 +1,35 @@
+package harrypotterkata.hogwarts.wizard.duelling;
+
+import harrypotterkata.hogwarts.wizard.Wizard;
+
+import static java.lang.String.format;
+
+class Announcer {
+    static void introduceBothWizards(Wizard attackingWizard, Wizard defendingWizard) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Welcome, wizards of Hogwarts, at the duelling club!");
+        sb.append("\n");
+        sb.append("\n");
+        sb.append(format("I want to introduce to you at our left side %s from house %s", attackingWizard.getName(), attackingWizard.getHouse().name().toLowerCase()));
+        sb.append("\n");
+        sb.append(format("The opponent at our right side is %s from house %s", defendingWizard.getName(), defendingWizard.getHouse().name().toLowerCase()));
+        sb.append("\n");
+        sb.append("\n");
+        sb.append("I wish both sides good luck and may the best wizard win!");
+
+        System.out.println(sb);
+    }
+
+    static void congratsWinner(Wizard winner) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\n");
+        sb.append("\n");
+        sb.append("We have a winner ladies and gentleman!");
+        sb.append("\n");
+        sb.append(format("%s from %s is the rightful winner", winner.getName(), winner.getHouse().name().toLowerCase()));
+
+        System.out.println(sb);
+    }
+}
--- src/main/java/harrypotterkata/hogwarts/wizard/duelling/DuellingClub.java
+++ src/main/java/harrypotterkata/hogwarts/wizard/duelling/DuellingClub.java
@@ -11,22 +11,11 @@ class DuellingClub {
     private Wizard winner;
 
     Wizard duel(Wizard a, Wizard b) {
-        introduceBothWizards(a, b);
-        winner = fight();
-        congratsWinner();
+        this.attackingWizard = a;
+        this.defendingWizard = b;
+        Announcer.introduceBothWizards(a, b);
+        Announcer.congratsWinner(fight());
         return winner;
-    }
-
-    private void congratsWinner() {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("\n");
-        sb.append("\n");
-        sb.append("We have a winner ladies and gentleman!");
-        sb.append("\n");
-        sb.append(format("%s from %s is the rightful winner", winner.getName(), winner.getHouse().name().toLowerCase()));
-
-        System.out.println(sb);
     }
 
     private Wizard fight() {
@@ -60,23 +49,5 @@ class DuellingClub {
 
     private boolean bothWizardsAreStillStanding() {
         return attackingWizard.getHp() > 0 && defendingWizard.getHp() > 0;
-    }
-
-    private void introduceBothWizards(Wizard a, Wizard b) {
-        this.attackingWizard = a;
-        this.defendingWizard = b;
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("Welcome, wizards of Hogwarts, at the duelling club!");
-        sb.append("\n");
-        sb.append("\n");
-        sb.append(format("I want to introduce to you at our left side %s from house %s", attackingWizard.getName(), attackingWizard.getHouse().name().toLowerCase()));
-        sb.append("\n");
-        sb.append(format("The opponent at our right side is %s from house %s", defendingWizard.getName(), defendingWizard.getHouse().name().toLowerCase()));
-        sb.append("\n");
-        sb.append("\n");
-        sb.append("I wish both sides good luck and may the best wizard win!");
-
-        System.out.println(sb);
     }
 }
