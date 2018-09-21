e70a63d7a540d1f5fb3ef0acc7df4615e43645c9
wimlens <wim.lens@cegeka.be>
2018-09-21 19:20:31 +0200

    Revert "introduce announcer"
    This reverts commit 50ae158cc5b444eec52c46031e655d05f4aa315e.

--- src/main/java/harrypotterkata/hogwarts/wizard/duelling/DuellingClub.java
+++ src/main/java/harrypotterkata/hogwarts/wizard/duelling/DuellingClub.java
@@ -13,8 +13,20 @@ class DuellingClub {
     Wizard duel(Wizard a, Wizard b) {
         introduceBothWizards(a, b);
         winner = fight();
-        Announcer.congratsWinner(winner);
+        congratsWinner();
         return winner;
+    }
+
+    private void congratsWinner() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\n");
+        sb.append("\n");
+        sb.append("We have a winner ladies and gentleman!");
+        sb.append("\n");
+        sb.append(format("%s from %s is the rightful winner", winner.getName(), winner.getHouse().name().toLowerCase()));
+
+        System.out.println(sb);
     }
 
     private Wizard fight() {
@@ -53,6 +65,18 @@ class DuellingClub {
     private void introduceBothWizards(Wizard a, Wizard b) {
         this.attackingWizard = a;
         this.defendingWizard = b;
-        Announcer.doIntro(a, b);
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Welcome, wizards of Hogwarts, at the duelling club!");
+        sb.append("\n");
+        sb.append("\n");
+        sb.append(format("I want to introduce to you at our left side %s from house %s", attackingWizard.getName(), attackingWizard.getHouse().name().toLowerCase()));
+        sb.append("\n");
+        sb.append(format("The opponent at our right side is %s from house %s", defendingWizard.getName(), defendingWizard.getHouse().name().toLowerCase()));
+        sb.append("\n");
+        sb.append("\n");
+        sb.append("I wish both sides good luck and may the best wizard win!");
+
+        System.out.println(sb);
     }
 }
